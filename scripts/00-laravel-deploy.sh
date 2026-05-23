fc4bb1611ae6a1c5fe1d1f004a26b99ebef9959e
vieryylovega1 <vieryylovega@gmail.com>
2026-05-23 11:10:57 +0700

    Gas Fixxing 502

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Starting Laravel deploy script..."
-
 composer install --optimize-autoloader --no-interaction --ignore-platform-reqs
 
 php artisan optimize:clear || true
-php artisan config:clear || true
-php artisan cache:clear || true
 php artisan route:clear || true
+php artisan config:clear || true
 php artisan view:clear || true
+php artisan cache:clear || true
 
 php artisan migrate --force || true
 php artisan storage:link || true
 
 chmod -R 775 storage bootstrap/cache || true
-
-echo "Laravel deploy script done."
