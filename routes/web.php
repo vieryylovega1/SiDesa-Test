c8fd997cdcc2c7bf9476bacd52f692276dfbe63f
vieryylovega1 <vieryylovega@gmail.com>
2026-05-23 11:54:42 +0700

    add render test route

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\VillageProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test-render', function () {
+    return 'Laravel Render Works';
+});
+
 Route::middleware('guest')->group(function () {
     Route::get('login', [AuthController::class, 'create'])->name('login');
     Route::post('login', [AuthController::class, 'store'])->name('login.store');
